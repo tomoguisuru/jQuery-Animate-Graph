f5a8230e1684c318a9ef2292e69d5555677a58e8
Brandon Drake <tomoguisuru@gmail.com>
2012-01-27 16:04:13 -0700

    Added the initial project files to the repo

---
+++ Demo_Project/scripts/json/json_graph.js
@@ -0,0 +1,53 @@
+{
+	"axis": [ 
+		"1996 Toyota Paseo", 
+		"1987 Mazda RX-7 TII", 
+		"2001 Subaru 2.5RS", 
+		"2007 Toyota Yaris S", 
+		"2001 Toyota Siena LE", 
+		"2005 Subaru Legacy 2.5GT Lim" 
+	],
+	"path": "images/metrics/",
+	"metrics" : [
+		{ 
+			"title" : "0-60 MPH", 
+			"name" : "0-60", 
+			"img" : "0-60.jpg",
+			"def": "Time to reach 60 miles per hour on a straight strech of road", 
+			"scaleMsg": "Seconds (Fewer is Better)", 
+			"results": [9.4, 6.5, 7.7, 8.8, 9.3, 5.23] 
+		},
+		{ 
+			"title" : "Quarter Mile", 
+			"name" : "1/4 Mile", 
+			"img" : "quarter-mile.jpg",
+			"def": "Time to complete a quarter mile race", 
+			"scaleMsg": "Seconds (Fewer is Better)", 
+			"results": [16.8, 15.0, 15.7, 16.8, 16.8, 13.95] 
+		},
+		{ 
+			"title" : "Average Gas MPG (City)", 
+			"name" : "City MPG", 
+			"img" : "mpg-city.jpg",
+			"def": "Average MPG you can expect while driving in the city", 
+			"scaleMsg": "Miles (More is Better)", 
+			"results": [24, 15, 18, 29, 17, 17] 
+		},
+		{ 
+			"title" : "Average Gas MPG (Highway)", 
+			"name" : "Highway MPG", 
+			"img" : "mpg-highway.jpg",
+			"def": "Average MPG you can expect while driving on the highway", 
+			"scaleMsg": "Miles (More is Better)", 
+			"results": [31, 21, 26, 36, 23, 23] 
+		},
+		{ 
+			"title" : "Horsepower", 
+			"name" : "HP", 
+			"img" : "horsepower.jpg",
+			"def": "Engine Horsepower", 
+			"scaleMsg": "Horsepower (More is Better)", 
+			"results": [93, 202, 155, 106, 210, 250] 
+		}
+	]
+}
